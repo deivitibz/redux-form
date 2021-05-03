78b4de72ef91dbad2d3f647198e12c5dc264dfd6
David Perez <dperezbe@everis.com>
2021-05-03 22:04:22 +0200

    add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+interface AppState {
+  message: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'redux-test';
+export class AppComponent implements OnInit{
+
+  message: Observable<string>;
+
+  constructor(private store: Store<AppState>){
+    this.message = this.store.select('message');
+  }
+
+  ngOnInit(){
+    this.spanishMessage()
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'})
+  }
 }
